77aaaf31da1f05bc73c60853422f39067312acea
habibakhan44 <152440245+habibakhan44@users.noreply.github.com>
2025-03-09 11:00:13 +0500

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,23 +1,40 @@
 import React from 'react'
-import {Home, Login} from './Store/screens';
-import { useDispatch, useSelector } from 'react-redux';
-import { login } from './Store/slices/auth.slice';
 
-// store= global state - accessible everywhere;
-// actions= functions jis mei hum apni logic likhein;
-// 
+import { Home, Login } from './screens';
+import { useDispatch, useSelector } from 'react-redux';
+import { login } from './store/slice/auth.slice';
+
+
 
 const App = () => {
-  const isLoggedIn= useSelector((state)=> state.auth.isLoggedIn) ;
-console.log(isLoggedIn)
-const dispatch= useDispatch();
+  const isLoggedIn = useSelector((state)=> state.auth.isLoggedIn);
+  console.log(isLoggedIn);
+  const dispatch = useDispatch();
+ 
+  
 
   return (
-    <div>
-      <Home   />
-      <Login />
+//     <div>
+     
+// <button onClick={()=> {dispatch(login())}}>Log In</button>
 
-      <button onClick={()=> {dispatch(login())}}>Log In</button>    </div>
+//     </div>
+
+    <div>
+     
+     {isLoggedIn ? <Home/> : <Login/>}
+
+     <button onClick={()=> {
+      isLoggedIn ? dispatch(logout()) : dispatch(login())
+      }}>
+      
+        {isLoggedIn ? "Logout" : "Login"}
+      
+      
+      </button>
+     
+    </div>
+
   )
 }
 
